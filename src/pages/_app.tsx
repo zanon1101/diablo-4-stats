0677f0703e6864c34b1df05eb4c4c4b487354313
Elliott <elliott@myconnect.com.au>
2023-06-10 13:58:58 +1000

    wrap in clerk provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { type AppType } from "next/app";
+
 import { api } from "~/utils/api";
+import { ClerkProvider } from "@clerk/nextjs/dist/types/components.server";
+
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />;
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
